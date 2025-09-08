f177a30197c56f32b64f0e550bff37997a3d6e64
Rabbit <night31916@gmail.com>
2025-09-08 14:54:15 +0800

    adjust path

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,18 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: '/',
+            redirect: '/login',
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../views/LoginView.vue'),
+        },
+        {
             path: '/todolist',
             name: 'todolist',
             component: () => import('../views/TodoListView.vue'),
-        },
-        {
-            path: '/',
-            name: 'login',
-            component: () => import('../views/LoginView.vue'),
         },
         {
             path: '/register',
@@ -21,14 +25,11 @@ const router = createRouter({
     ],
 })
 
-router.beforeEach(async (to, from, next) => {
+router.beforeEach((to, from, next) => {
     const token = document.cookie.replace(/(?:^|.*;\s*)todolistToken\s*=\s*([^;]*).*$/i, '$1')
-    if (!token) {
-        if (to.path === '/' || to.path === '/register') {
-            return next()
-        } else {
-            return next('/')
-        }
+
+    if (!token && to.path !== '/login' && to.path !== '/register') {
+        return next('/login')
     }
     next()
 })
